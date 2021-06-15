020be455f7c2d4e38e0f8ee8a2bc4771da2a3993
LonTech99 <madelon148073@hotmail.nl>
2021-06-15 14:13:48 +0200

    update respond
    update respond

--- main.py
+++ main.py
@@ -1,36 +1,82 @@
+from enum import auto
 import speech_recognition as sr
 import webbrowser
-from time import ctime
+import playsound
+import os
+import random
+from gtts import gTTS
+import time
 
-r = sr.Recognizer()
+class person:
+    name = ''
+    def setName(self, name):
+        self.name = name
 
-def record_audio(ask = False):
+def there_exists(terms):
+    for term in terms:
+        if term in voice_data:
+            return True
 
-    with sr.Microphone() as source:
+r = sr.Recognizer() # initialise a recogniser
+# listen for audio and convert it to text:
+def record_audio(ask=False):
+    with sr.Microphone() as source: # microphone as source
         if ask:
-            print(ask)
-        audio = r.listen(source)
+            alexis_speak(ask)
+        audio = r.listen(source)  # listen for the audio via source
         voice_data = ''
         try:
-            voice_data = r.recognize_google(audio)
-            print(voice_data)
-        except sr.UnknownValueError:
-            print('Sorry, ik heb dat niet begrepen')
+            voice_data = r.recognize_google(audio)  # convert audio to text
+        except sr.UnknownValueError: # error: recognizer does not understand
+            alexis_speak('I did not get that')
         except sr.RequestError:
-            print('Sorry, er is iets misgegaan')
-        return voice_data
+            alexis_speak('Sorry, the service is down') # error: recognizer is not connected
+        print(f">> {voice_data.lower()}") # print what user said
+        return voice_data.lower()
+
+def alexis_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='nl')    
+    r = random.randint(1, 100000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)  
+    print(audio_string)
+    os.remove(audio_file) 
 
 def respond(voice_data):
-    if 'what is your name' in voice_data:
-        print('My name is test')
-    if 'what time is it' in voice_data:
-        print(ctime())
+        # 1: greeting
+    if there_exists(['hoi','hey','hallo']):
+        greetings = [f"goedendag, hoe gaat het? {person_obj.name}", f"Hallo, hoe gaat het met u? {person_obj.name}", f"Waar kan ik u mee helpen? {person_obj.name}", f"Kan ik u ergens mee helpen? {person_obj.name}"]
+        greet = greetings[random.randint(0,len(greetings)-1)]
+        alexis_speak(greet)
+
+            # 2: name
+    if there_exists(["Wat is uw naam?"]):
+        if person_obj.name:
+            alexis_speak("Ik ben de Voice assistent van de Gemeente Rotterdam")
+        else:
+            alexis_speak("Ik ben de Voice assistent van de Gemeente Rotterdam en wie bent u?")
+
+    if there_exists(["mijn naam is"]):
+        person_name = voice_data.split("is")[-1].strip()
+        alexis_speak(f"Mooie naam {person_name}")
+        person_obj.setName(person_name) # remember name in person object
+
+    # 3: greeting
+    if there_exists(["Hoe is het","Hoe gaat het met je"]):
+        alexis_speak(f"Gaat zeer goed, bedankt voor het vragen {person_obj.name}")
+    if 'Waar ben ik nu?' in voice_data:
+        alexis_speak('In Rotterdam')
+    if 'Wat kan ik hier precies doen?' in voice_data:
+        alexis_speak('Uw gemeentezaken regelen')
     if 'search' in voice_data:
-        search = record_audio('What do you want to search for')        
+        search = record_audio('Waar wil u naar zoeken')        
         url = 'https://google.com/search?q=' + search
         webbrowser.get().open(url)
-        print('Dit is wat ik vond' + search)
+        alexis_speak('Dit is wat ik vond' + search)
 
-print('Hoe kan ik je helpen?')
-voice_data = record_audio()
-respond(voice_data)
+time.sleep(1)
+person_obj = person()
+while(1):
+    voice_data = record_audio() # get the voice input
+    respond(voice_data) # respond
